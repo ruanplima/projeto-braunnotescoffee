af5924c355b1f4bcfac845981829af61942c8813
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2023-12-14 18:27:07 -0300

    Update no projeto
    Adicionado Cardápio Modal

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const btn = document.querySelector('.btn-menu')
-const menu = document.querySelector('ul')
+function openMenu(){
+    const btn = document.querySelector('.btn-menu')
+    const menu = document.querySelector('ul')
 
-function toggleMenu(){
-    menu.classList.toggle('ativo')
+    function toggleMenu(){
+        menu.classList.toggle('ativo')
+    }
+
+
+    btn.addEventListener('click', toggleMenu)
 }
+openMenu()
 
+function modal(){
+    const btnModal = document.querySelector('.openModal')
+    const modal = document.querySelector('.modal')
+    const icon = document.querySelector('.fa-circle-xmark')
+    
+    function openModal(){
+        modal.classList.add('ativo')
+        modal.addEventListener('click', (e) => {
+            if(e.target.id == 'modal'){
+                modal.classList.remove('ativo')
+            }
+        })
+    }
 
-btn.addEventListener('click', toggleMenu)
+    function fecharModal(){
+        modal.classList.remove('ativo')
+    }
+
+    btnModal.addEventListener('click', openModal)
+    icon.addEventListener('click', fecharModal)
+}
+modal()
